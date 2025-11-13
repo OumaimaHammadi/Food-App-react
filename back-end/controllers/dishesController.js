8adbdafeac15c9a3748d45873b940b5896b9ecc9
oumaima <hammadiioumaima@gmail.com>
2025-11-13 05:05:30 +0100

    change

--- back-end/controllers/dishesController.js
+++ back-end/controllers/dishesController.js
@@ -1,14 +1,28 @@
-import Dishe from '../models/Dishes.js'
+// import Dishe from '../models/Dishes.js'
 
 
 
-export const dishes = async(req,res)=>{
-let dishes = await Dishe.find({})
-let newdishes= dishes.slice(0,6)
+// export const dishes = async(req,res)=>{
+// let dishes = await Dishe.find({})
+// let newdishes= dishes.slice(0,6)
 
-console.log("Dishe Fetched")
-res.send(newdishes)
+// console.log("Dishe Fetched")
+// res.send(newdishes)
 
-}
+// }
+
+export const dishes = async (req, res) => {
+  try {
+    let dishes = await Dishe.find({});
+    let newdishes = dishes.slice(0, 6);
+
+    console.log("Dishe Fetched:", newdishes.length);
+    res.send(newdishes);
+  } catch (err) {
+    console.error("Error fetching dishes:", err);
+    res.status(500).send({ error: "Database fetch failed" });
+  }
+};
+
 
 
